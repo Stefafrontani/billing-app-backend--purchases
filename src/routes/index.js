662bc3e40643aeb56d8ca15e70402a7055a3949c
stefafrontani <stefafrontani@gmail.com>
2021-08-25 12:21:53 -0300

    change routes order

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const { Router } =  require('express')
 const router = Router()
 const { getPurchaseById, getTotalPurchasesAmount, createPurchase } = require('../controllers/index.controller')
 
-router.get('/purchases/:purchaseId', getPurchaseById)
-router.get('/purchases/amounts', getTotalPurchasesAmount)
 router.post('/purchases', createPurchase)
+router.get('/purchases/amounts', getTotalPurchasesAmount)
+router.get('/purchases/:purchaseId', getPurchaseById)
 
 module.exports = router;
